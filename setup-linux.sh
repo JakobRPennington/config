7469b975d8bc93d182c74e3a0e4825fe8c9f1e7d
Jakob Pennington <jakob@pennington.io>
2020-02-27 11:36:47 +1030

    Added setup script, updated configs

---
+++ setup-linux.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check for root
+if ! [ $(id -u) = 0 ]; then
+   echo "Run me again as root."
+   exit 1
+fi
+
+# Install packages
+apt install -y zsh
+
+# Change default shell to zsh
+chsh --shell /bin/zsh $(whoami)
+
+# Copy zsh config
+cp ./zsh/.zshrc ~/.zshrc
+
+# Install powerlevel9k theme
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+# Set up oh-my-zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
