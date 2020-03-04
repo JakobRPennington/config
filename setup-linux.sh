eea9f0b086bfbbfaf3f8a01efc051ceb58f40c47
Jakob Pennington <jakob@pennington.io>
2020-03-04 19:15:16 +1030

    Fix to zsh config

--- setup-linux.sh
+++ setup-linux.sh
@@ -9,14 +9,14 @@ fi
 # Install packages
 apt-get install -y zsh
 
-# Copy zsh config
-cp ./zsh/.zshrc ~/.zshrc
-
 # Set up oh-my-zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install powerlevel9k theme
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+# Copy zsh config
+cp ./zsh/.zshrc ~/.zshrc
 
 # Change default shell to zsh
 chsh --shell $(which zsh) $(whoami)
